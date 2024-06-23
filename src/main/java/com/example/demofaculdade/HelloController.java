9b873b298dda0042c47b215e9423eb6091e49741
Renee Barros <reneebarros4@gmail.com>
2024-06-22 23:25:44 -0300

    Desafio Contador

---
+++ src/main/java/com/example/demofaculdade/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demofaculdade;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
